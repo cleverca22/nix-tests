e7f2ebb5183416b2df50a75541c3eb77ec230c27
michael bishop <cleverca22@gmail.com>
2016-12-09 01:34:06 -0400

    adding a kexec tarball

---
+++ kexec/configuration.nix
@@ -0,0 +1,45 @@
+# new cmd: nix-build '<nixpkgs/nixos>' -A config.system.build.kexec_tarball -I nixos-config=./configuration.nix -Q -j 4
+# old cmd: nix-build -I nixpkgs=/home/clever/apps/nixpkgs/ '<nixpkgs/nixos>' -A config.system.build.kexec_script -I nixos-config=/home/clever/apps/nixpkgs/configuration.nix
+
+{ lib, pkgs, config, ... }:
+
+with lib;
+
+let
+  image = pkgs.runCommand "image" { buildInputs = [ pkgs.nukeReferences ]; } ''
+    mkdir $out
+    cp ${config.system.build.kernel}/bzImage $out/kernel
+    cp ${config.system.build.netbootRamdisk}/initrd $out/initrd
+    nuke-refs $out/kernel
+  '';
+in {
+  imports = [ <nixpkgs/nixos/modules/installer/netboot/netboot-minimal.nix> ];
+  system.build.kexec_script = pkgs.writeTextFile {
+    executable = true;
+    name = "kexec-nixos";
+    text = ''
+      #!${pkgs.stdenv.shell}
+      export PATH=${pkgs.kexectools}/bin:$PATH
+      kexec -l ${image}/kernel --initrd=${image}/initrd --append="init=${builtins.unsafeDiscardStringContext config.system.build.toplevel}/init ${toString config.boot.kernelParams}"
+      sync
+      echo "executing kernel, filesystems will be improperly umounted"
+      kexec -e
+    '';
+  };
+  system.build.kexec_tarball = pkgs.callPackage <nixpkgs/nixos/lib/make-system-tarball.nix> {
+    storeContents = [
+      { object = config.system.build.kexec_script; symlink = "/kexec_nixos"; }
+    ];
+    contents = [];
+  };
+  boot.loader.grub.enable = false;
+  boot.kernelParams = [ "console=ttyS0,115200" ];
+  systemd.services.sshd.wantedBy = mkForce [ "multi-user.target" ];
+  networking.hostName = "kexec";
+  users.users.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC34wZQFEOGkA5b0Z6maE3aKy/ix1MiK1D0Qmg4E9skAA57yKtWYzjA23r5OCF4Nhlj1CuYd6P1sEI/fMnxf+KkqqgW3ZoZ0+pQu4Bd8Ymi3OkkQX9kiq2coD3AFI6JytC6uBi6FaZQT5fG59DbXhxO5YpZlym8ps1obyCBX0hyKntD18RgHNaNM+jkQOhQ5OoxKsBEobxQOEdjIowl2QeEHb99n45sFr53NFqk3UCz0Y7ZMf1hSFQPuuEC/wExzBBJ1Wl7E1LlNA4p9O3qJUSadGZS4e5nSLqMnbQWv2icQS/7J8IwY0M8r1MsL8mdnlXHUofPlG1r4mtovQ2myzOx clever@nixos" ];
+  nixpkgs.config.packageOverrides = pkgs: {
+    kexectools = pkgs.kexectools.overrideDerivation (old: {
+      hardeningDisable = [ "all" ];
+    });
+  };
+}
---
+++ kexec/session.md
@@ -0,0 +1,34 @@
+$ nix-build '<nixpkgs/nixos>' -A config.system.build.kexec_tarball -I nixos-config=./configuration.nix -Q -j 4 
+$ scp result/tarball/nixos-system-x86_64-linux.tar.xz 192.168.2.151:.
+$ ssh 192.168.2.151
+Welcome to Ubuntu 16.10 (GNU/Linux 4.8.0-22-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+49 packages can be updated.
+29 updates are security updates.
+
+clever@ubuntu:~$ sudo -i
+[sudo] password for clever:
+root@ubuntu:~# cd /
+root@ubuntu:/# tar -xf /home/clever/nixos-system-x86_64-linux.tar.xz
+root@ubuntu:/# ./kexec_nixos 
+executing kernel, filesystems will be improperly umounted
+Connection to 192.168.2.151 closed.
+[clever@amd-nixos:~/nix-tests/kexec]$ ping 192.168.2.151
+PING 192.168.2.151 (192.168.2.151) 56(84) bytes of data.
+64 bytes from 192.168.2.151: icmp_seq=1 ttl=64 time=0.197 ms
+64 bytes from 192.168.2.151: icmp_seq=2 ttl=64 time=0.121 ms
+64 bytes from 192.168.2.151: icmp_seq=3 ttl=64 time=0.181 ms
+^C
+[clever@amd-nixos:~/nix-tests/kexec]$ ssh root@192.168.2.151
+The authenticity of host '192.168.2.151 (192.168.2.151)' can't be established.
+ED25519 key fingerprint is SHA256:o1Tl49CuK6Ipd5gT6GaNfotsgVMJcdxr2FZbGrmhqmE.
+Are you sure you want to continue connecting (yes/no)? yes
+Warning: Permanently added '192.168.2.151' (ED25519) to the list of known hosts.
+Last login: Fri Dec  9 05:47:11 2016
+
+[root@kexec:~]# 
+
