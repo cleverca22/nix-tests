aca0a0bb06f1336523a58d57a3fd30e99a4b6364
michael bishop <cleverca22@gmail.com>
2017-02-14 12:23:19 -0400

    add an auto-reboot helper, remove a fix for kexec since its now in nixpkgs, add zfs support

--- kexec/configuration.nix
+++ kexec/configuration.nix
@@ -19,7 +19,7 @@ in {
     name = "kexec-nixos";
     text = ''
       #!${pkgs.stdenv.shell}
-      export PATH=${pkgs.kexectoolsFixed}/bin:${pkgs.cpio}/bin:$PATH
+      export PATH=${pkgs.kexectools}/bin:${pkgs.cpio}/bin:$PATH
       set -x
       cd $(mktemp -d)
       pwd
@@ -40,6 +40,7 @@ in {
     mkdir -p /mnt-root/root/.ssh/
     cp /authorized_keys /mnt-root/root/.ssh/
   '';
+  boot.supportedFilesystems = [ "zfs" ];
   system.build.kexec_tarball = pkgs.callPackage <nixpkgs/nixos/lib/make-system-tarball.nix> {
     storeContents = [
       { object = config.system.build.kexec_script; symlink = "/kexec_nixos"; }
@@ -49,12 +50,15 @@ in {
   boot.loader.grub.enable = false;
   boot.kernelParams = [ "console=ttyS0,115200" ];
   systemd.services.sshd.wantedBy = mkForce [ "multi-user.target" ];
+  systemd.timers.autoreboot = {
+    partOf = [ "autoreboot.service" ];
+    wantedBy = [ "timers.target" ];
+    timerConfig.OnCalendar = "hourly";
+  };
+  systemd.services.autoreboot = {
+    script = "shutdown -r +5";
+  };
   networking.hostName = "kexec";
   # example way to embed an ssh pubkey into the tar
   # users.users.root.openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC34wZQFEOGkA5b0Z6maE3aKy/ix1MiK1D0Qmg4E9skAA57yKtWYzjA23r5OCF4Nhlj1CuYd6P1sEI/fMnxf+KkqqgW3ZoZ0+pQu4Bd8Ymi3OkkQX9kiq2coD3AFI6JytC6uBi6FaZQT5fG59DbXhxO5YpZlym8ps1obyCBX0hyKntD18RgHNaNM+jkQOhQ5OoxKsBEobxQOEdjIowl2QeEHb99n45sFr53NFqk3UCz0Y7ZMf1hSFQPuuEC/wExzBBJ1Wl7E1LlNA4p9O3qJUSadGZS4e5nSLqMnbQWv2icQS/7J8IwY0M8r1MsL8mdnlXHUofPlG1r4mtovQ2myzOx clever@nixos" ];
-  nixpkgs.config.packageOverrides = pkgs: {
-    kexectoolsFixed = pkgs.kexectools.overrideDerivation (old: {
-      hardeningDisable = [ "all" ];
-    });
-  };
 }
