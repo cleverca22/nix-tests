08f0fffcc3b9ee2c6bceb1c8b439703c676fe3ca
michael bishop <cleverca22@gmail.com>
2017-05-16 23:50:03 -0300

    add another example

--- fs-test/configuration.nix
+++ fs-test/configuration.nix
@@ -5,6 +5,8 @@ let
   volumes = [ "/" "/nix" "/home" ];
 in {
   boot.loader.grub.device = "/dev/sda";
-  fileSystems = map (v: { device = "${pool}${v}"; mountPoint = v; fsType = "zfs"; }) volumes;
+  fileSystems = [
+    { device = "/dev/sda1"; mountPoint = "/data"; fsType = "xfs"; }
+  ] ++ (map (v: { device = "${pool}${v}"; mountPoint = v; fsType = "zfs"; }) volumes);
   networking.hostId = "fe1f6cbf";
 }
