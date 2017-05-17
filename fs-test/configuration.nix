2b96681521c20dba636a329712990765e150a1b6
michael bishop <cleverca22@gmail.com>
2017-05-16 23:51:33 -0300

    fix rootfs

--- fs-test/configuration.nix
+++ fs-test/configuration.nix
@@ -2,11 +2,12 @@
 
 let
   pool = "tank";
-  volumes = [ "/" "/nix" "/home" ];
+  volumes = [ "/nix" "/home" ];
 in {
   boot.loader.grub.device = "/dev/sda";
   fileSystems = [
     { device = "/dev/sda1"; mountPoint = "/data"; fsType = "xfs"; }
+    { device = "${pool}/root"; mountPoint = "/"; fsType = "zfs"; }
   ] ++ (map (v: { device = "${pool}${v}"; mountPoint = v; fsType = "zfs"; }) volumes);
   networking.hostId = "fe1f6cbf";
 }
