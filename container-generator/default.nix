78cfab2d65cc0a6b80d157548ef700d94cf82db3
michael bishop <cleverca22@gmail.com>
2017-06-15 15:40:54 -0300

    add an unshare helper

--- container-generator/default.nix
+++ container-generator/default.nix
@@ -1,11 +1,16 @@
-with import <nixpkgs> {};
-
 let
   eval = import <nixpkgs/nixos> { configuration = ./configuration.nix; };
+  pkgs = eval.config._module.args.pkgs;
+  enterScript = pkgs.writeScript "enter" ''
+    #!${pkgs.stdenv.shell}
+    export PATH=${eval.config.system.path}/bin/
+    exec unshare -p ${eval.config.system.build.toplevel}/init
+  '';
 in pkgs.callPackage <nixpkgs/nixos/lib/make-system-tarball.nix> {
   storeContents = [
     { object = "${eval.config.system.build.toplevel}/init"; symlink = "/init"; }
-    { object = "${eval.config._module.args.pkgs.bashInteractive}/bin/bash"; symlink = "/bash"; }
+    { object = "${pkgs.bashInteractive}/bin/bash"; symlink = "/bash"; }
+    { object = enterScript; symlink = "/enter"; }
   ];
   contents = [];
 }
