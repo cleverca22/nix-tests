0998c416af8a0d62f700147931a2c9f62af45010
michael bishop <cleverca22@gmail.com>
2017-06-16 17:12:27 -0300

    this should fix nix trying to remove itself

--- container-generator/configuration.nix
+++ container-generator/configuration.nix
@@ -4,4 +4,18 @@
   boot.isContainer = true;
   networking.hostName = "guest";
   networking.dhcpcd.enable = false;
+    boot.postBootCommands =
+      ''
+        # After booting, register the contents of the Nix store on the
+        # CD in the Nix database in the tmpfs.
+        if [ -f /nix-path-registration ]; then
+          ${config.nix.package.out}/bin/nix-store --load-db < /nix-path-registration &&
+          rm /nix-path-registration
+        fi
+
+        # nixos-rebuild also requires a "system" profile and an
+        # /etc/NIXOS tag.
+        touch /etc/NIXOS
+        ${config.nix.package.out}/bin/nix-env -p /nix/var/nix/profiles/system --set /run/current-system
+      '';
 }
