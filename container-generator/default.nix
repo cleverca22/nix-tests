c05f8deae6c0d6b9488acebc53cf2b5e3e788acc
michael bishop <cleverca22@gmail.com>
2017-06-16 19:02:19 -0300

    oops

--- container-generator/default.nix
+++ container-generator/default.nix
@@ -2,6 +2,7 @@ let
   eval = import <nixpkgs/nixos> { configuration = ./configuration.nix; };
   pkgs = eval.config._module.args.pkgs;
 in rec {
+  tarball = eval.config.system.build.tarball;
   cfg = { ... }: {
     services.mingetty.autologinUser = "root";
     virtualisation.memorySize = 2048;
